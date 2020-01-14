3736f0489dfaeb4c11e9f234ba058a968695ce57
Jay <jrim@my.bcit.ca>
2020-01-14 11:09:17 -0800

    lab1 race initial commit with hare and tortoise object.

---
+++ src/ca/bcit/comp2522/labs/lab01/Race.java
@@ -0,0 +1,8 @@
+package ca.bcit.comp2522.labs.lab01;
+
+public class Race {
+    public static void main(String[] args) {
+        Hare hareObject = new Hare();
+        Tortoise TortoiseObject = new Tortoise();
+    }
+}
