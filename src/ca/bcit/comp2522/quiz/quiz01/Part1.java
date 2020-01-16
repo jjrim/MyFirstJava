371b8e40a053a610e42b7ccfdd6f173ab721f271
Jay <jrim@my.bcit.ca>
2020-01-16 15:23:40 -0800

    Jay Rim and Erica Jeong

--- src/ca/bcit/comp2522/quiz/quiz01/Part1.java
+++ src/ca/bcit/comp2522/quiz/quiz01/Part1.java
@@ -20,16 +20,16 @@ public class Part1 {
             minutes.add(i);
         }
 
-        for (int hour : hours) {
-            if (hour == 1) {
+        for (int hour = 0; hour < hours.size(); i++) {
+            if (hours.get(hour) == 1) {
                 answer += 2;
-            } else if (hour == 2 || hour == 3 || hour == 5) {
+            } else if (hours.get(hour) == 2 || hours.get(hour) == 3 || hours.get(hour) == 5) {
                 answer += 5;
-            } else if (hour == 4) {
+            } else if (hours.get(hour) == 4) {
                 answer += 4;
-            } else if (hour == 6 || hour == 0 || hour == 9) {
+            } else if (hours.get(hour) == 6 || hours.get(hour) == 0 || hours.get(hour) == 9) {
                 answer += 6;
-            } else if (hour == 8)
+            } else if (hours.get(hour) == 8)
                 answer += 7;
         }
 
